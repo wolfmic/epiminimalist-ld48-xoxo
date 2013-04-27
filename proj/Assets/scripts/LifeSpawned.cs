257016b0b03e885d040022441383725004f6e5e6
Aymeric Girault <aymeric.girault@epitech.eu>
2013-04-27 22:51:28 +0200

    la vie est belle

--- proj/Assets/scripts/LifeSpawned.cs
+++ proj/Assets/scripts/LifeSpawned.cs
@@ -4,9 +4,22 @@ using System.Collections;
 public class LifeSpawned : MonoBehaviour {
 	
 	public int limit_y = 0;
-	
+	private float timeout = 0;
+	private bool timeActivatedJump = false;
+	private bool timeActivatedDestroy = false;
+			
 	// Use this for initialization
 	void Start () {
+	}
+	
+	public void timeOutJump (float t) {
+		timeout = t;
+		timeActivatedJump = true;
+	}
+	
+	public void timeOutDestroy (float t) {
+		timeout = t;
+		timeActivatedDestroy = true;
 	}
 	
 	// Update is called once per frame
@@ -14,6 +27,21 @@ public class LifeSpawned : MonoBehaviour {
 		if (this.transform.position.y <= limit_y)
 		{
 			Destroy(this.gameObject);
+		}
+		timeout -= Time.deltaTime;
+		if (timeActivatedDestroy == true) {
+			if (timeout <= 0) {
+				Destroy(this.gameObject);
+				timeActivatedDestroy = false;
+				timeout = 0;
+			}
+		}
+		if (timeActivatedJump == true) {
+			if (timeout <= 0) {
+				timeActivatedJump = false;
+				this.rigidbody.drag = 1;
+				timeout = 0;
+			}
 		}
 	}
 }
